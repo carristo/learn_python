c3264de5e2dc755ace64bec25b252cd52dee944a
carristo <carrristo@mail.ru>
2023-02-15 13:44:45 +0300

    Added new task for substring replacement

---
+++ Replacement/replacement.py
@@ -0,0 +1,10 @@
+def replace_first_8_symbols(initial_str : str) -> str:
+    return f'autotest{initial_str[8:len(initial_str)]}'
+
+def replace_all_until_first_dash(initial_str : str) -> str:
+    i = initial_str.find("-")
+    return f'autotest{initial_str[i:len(initial_str)]}'
+
+
+print(replace_first_8_symbols("a1a1a1a1-a1a1-a1a1-a1a1a1a1a1a1"))
+print(replace_all_until_first_dash("b1b1b1b1-b1b1-b1b1-b1b1b1b1b1b1"))
